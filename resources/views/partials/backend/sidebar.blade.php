99a478fe84d216a7c450097c5c4367b1f4ac8c4a
kapiljangid1996 <kapiljangid199276@gmail.com>
2022-04-12 11:21:38 +0530

    12-April-2022

--- resources/views/partials/backend/sidebar.blade.php
+++ resources/views/partials/backend/sidebar.blade.php
@@ -3,22 +3,33 @@
     <div id="dismiss" class="d-lg-none"><i class="flaticon-cancel-12"></i></div>
     
     <nav id="sidebar">
-        <ul class="navbar-nav theme-brand flex-row  d-none d-lg-flex">
-            <li class="nav-item d-flex">
-                <a href="index.html" class="navbar-brand">
+        <ul class="navbar-nav theme-brand text-center">
+            <li class="nav-item">
+                <a href="{{ url('/admin') }}" class="navbar-brand">
                     <img src="{{ asset('backend/assets/img/logo-3.png') }}" class="img-fluid" alt="logo">
                 </a>
-                <p class="border-underline"></p>
-            </li>
-            <li class="nav-item theme-text">
-                <a href="index.html" class="nav-link"> Equation </a>
             </li>
         </ul>
         <ul class="list-unstyled menu-categories" id="accordionExample">
             <li class="menu">
-                <ul class="collapse submenu list-unstyled show" id="dashboard" data-parent="#accordionExample">
+                <ul class="collapse submenu list-unstyled show">
                     <li class="active">
                         <a href="{{ url('/admin') }}"> <i class="flaticon-computer-4"></i> Dashboard </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="menu">
+                <a href="#frontend" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <i class="flaticon-home-fill"></i>
+                        <span>Home Page Setting</span>
+                        <i class="flaticon-right-arrow"></i>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="frontend" data-parent="#accordionExample">
+                    <li>
+                        <a href="{{ url('/admin/sliders') }}"> Slider </a>
                     </li>
                 </ul>
             </li>
